e7099102615934321d199ea82548b0079e87405e
Neokrid <sashachernov2019@mail.ru>
2026-02-22 00:05:38 +0700

    Authentication

---
+++ pkg/constants/constans.go
@@ -0,0 +1,25 @@
+package constants
+
+//Status
+const (
+	StatusCreated    = "created"
+	StatusInProgress = "in_progress"
+	StatusDone       = "done"
+)
+
+// Context
+const (
+	UserIdCtx    = "userId"
+	UserRoleCtx  = "userRole"
+	RequestIdCtx = "requestId"
+	TraceIdCtx   = "traceId"
+	SpanIdCtx    = "spanId"
+	ApiNameCtx   = "apiName"
+)
+
+// Errors
+const (
+	BindBodyError      string = "bind_body"
+	BindPathError      string = "bind_path"
+	UserIdTypeMismatch string = "type_mismatch"
+)
